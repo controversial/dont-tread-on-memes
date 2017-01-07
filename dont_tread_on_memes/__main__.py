3ff4aef8d130cdcbf149328d93337fa984a9a94b
Luke Deen Taylor <luke@deentaylor.com>
2017-01-07 01:29:16 -0500

    Allow saving via --save CLI parameter

--- dont_tread_on_memes/__main__.py
+++ dont_tread_on_memes/__main__.py
@@ -6,8 +6,13 @@ import click
 @click.option("--message", prompt="Don't _____ me: ",
               help=("The word or phrase to substitute for 'tread' in 'don't "
                     "tread on me'"))
-def tread(message):
-    dont_tread_on_memes.tread_on(message).show()
+@click.option("--save", default=None, help="Where to save the image")
+def tread(message, save):
+    flag = dont_tread_on_memes.tread_on(message)
+    if save is not None:
+        flag.save(save)
+    else:
+        flag.show()
 
 
 if __name__ == "__main__":
