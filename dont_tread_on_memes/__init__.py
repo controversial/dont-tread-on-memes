2da8299dbb1d47ecfb68dd45fcd79da4b55c28fe
Luke Deen Taylor <luke@deentaylor.com>
2017-01-07 01:09:07 -0500

    Add main program

---
+++ dont_tread_on_memes/__init__.py
@@ -0,0 +1,23 @@
+#!python3
+
+import os
+
+from PIL import Image, ImageDraw, ImageFont
+
+BLANK_FLAG = Image.open(
+    os.path.join(os.path.dirname(__file__), "dont-tread-on-blank.png")
+)
+
+
+def tread_on(phrase):
+    """Caption the "Don't Tread on Me" snake with `phrase`"""
+    flag = BLANK_FLAG.copy()
+    draw = ImageDraw.Draw(flag)
+    font = ImageFont.truetype("Times New Roman", 120)
+
+    text = "Don't {} me".format(phrase)
+    font_pos = (flag.width / 2 - font.getsize(text)[0] / 2, 1088)
+
+    draw.text(font_pos, text, font=font, fill="black")
+
+    return flag
