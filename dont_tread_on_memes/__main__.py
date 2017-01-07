d968a1b4ada3e27bd9613db873b5329c349464af
Luke Deen Taylor <luke@deentaylor.com>
2017-01-07 10:58:55 -0500

    Better help text

--- dont_tread_on_memes/__main__.py
+++ dont_tread_on_memes/__main__.py
@@ -4,13 +4,16 @@ import click
 
 @click.command()
 @click.option("--message", prompt="Don't _____ me",
-              help=("The word or phrase to substitute for 'tread' in 'don't "
-                    "tread on me'"))
+              help=("A message to use as a caption for the flag"),
+              metavar="<caption>")
 @click.option("--format/--no-format", default=True,
-              help=("Use the provided message as the entire caption instead of"
-                    " formatting it as 'Don't [message] me'"))
+              help=("Whether to format the provided message as 'Don't "
+                    "<caption> me' (default) or to use the provided message "
+                    "as the entire caption."))
 @click.option("--save", default=None, help="Where to save the image")
 def tread(message, format, save):
+    """Create a Don't Tread on Meme — an image of the Gadsden flag with the
+    words "DONT TREAD ON ME" replaced by something else."""
     # Generate the flag
     if format is True:
         flag = dont_tread_on_memes.dont_me(message)
