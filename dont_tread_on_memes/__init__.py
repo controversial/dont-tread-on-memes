936923f470fff4d8f245bd85d41ebe58370216e4
Luke Deen Taylor <luke@deentaylor.com>
2017-01-07 10:31:50 -0500

    Another method

--- dont_tread_on_memes/__init__.py
+++ dont_tread_on_memes/__init__.py
@@ -9,16 +9,21 @@ BLANK_FLAG = Image.open(
 )
 
 
-def tread_on(phrase):
-    """Caption the "Don't Tread on Me" snake with `phrase`"""
+def tread_on(caption):
+    """Caption the "Don't Tread on Me" snake with `caption`"""
     flag = BLANK_FLAG.copy()
     draw = ImageDraw.Draw(flag)
     font = ImageFont.truetype("Times New Roman", 120)
 
-    text = "don't {} me".format(phrase).upper()
+    text = caption.upper()
 
     font_pos = (flag.width / 2 - font.getsize(text)[0] / 2, 1088)
 
     draw.text(font_pos, text, font=font, fill="black")
 
     return flag
+
+
+def dont_me(phrase):
+    """Caption the "Don't tread on me" flag with "Don't [phrase] me" """
+    return tread_on("don't {} me".format(phrase))
