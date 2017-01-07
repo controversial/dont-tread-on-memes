dad86f0637ea94abf1cdbf6674b62696980d5589
Luke Deen Taylor <luke@deentaylor.com>
2017-01-07 10:47:55 -0500

    Allow 'raw' captioning via the --no-format flag

--- dont_tread_on_memes/__main__.py
+++ dont_tread_on_memes/__main__.py
@@ -6,9 +6,18 @@ import click
 @click.option("--message", prompt="Don't _____ me",
               help=("The word or phrase to substitute for 'tread' in 'don't "
                     "tread on me'"))
+@click.option("--format/--no-format", default=True,
+              help=("Use the provided message as the entire caption instead of"
+                    " formatting it as 'Don't [message] me'"))
 @click.option("--save", default=None, help="Where to save the image")
-def tread(message, save):
-    flag = dont_tread_on_memes.tread_on(message)
+def tread(message, format, save):
+    # Generate the flag
+    if format is True:
+        flag = dont_tread_on_memes.dont_me(message)
+    else:
+        flag = dont_tread_on_memes.tread_on(message)
+
+    # Save or show
     if save is not None:
         flag.save(save)
     else:
